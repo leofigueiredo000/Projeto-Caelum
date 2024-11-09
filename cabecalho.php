9737f14b621754a66734e4b67313936d356fcade
leot120 <leozim543@gmail.com>
2024-11-08 23:33:50 -0300

    cap 7 fim

--- cabecalho.php
+++ cabecalho.php
@@ -8,6 +8,7 @@
   <link rel="stylesheet" href="css/reset.css" />
   <link rel="stylesheet" href="css/estilos.css" />
   <link rel="stylesheet" href="css/sobre.css">
+  <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
 
   <title> <?php print $cabecalho_title; ?></title>
 </head>
