09c640c77a014103e440acff36d8251f2e83d674
leot120 <leozim543@gmail.com>
2024-11-11 17:27:23 -0300

    cap 8

--- produto.php
+++ produto.php
@@ -1,13 +1,45 @@
 </head>
 
 <body>
-  
+
   <?php
   $cabecalho_title = "Produto da Mirror Fashion";
   include("cabecalho.php");
   ?>
-  
-  <!-- aqui vai vir o miolo da pagina depois -->
+
+  <div class="produto">
+
+    <h1>Fuzzy Cardigan</h1>
+    <p>Por apenas R$ 129,00</p>
+  </div>
+
+  <form>
+    <fieldset class="cores">
+      <legend>Escolha a cor:</legend>
+
+      <input type="radio" name="cor" id="verde" value="verde" checked>
+      <label for="verde">
+        <img src="img/produtos/foto2-verde.png" alt="verde">
+      </label>
+
+      <input type="radio" name="cor" id="rosa" value="rosa checked">
+      <label for="rosa">
+        <img src="img/produtos/foto2-rosa.png" alt="rosa">
+      </label>
+
+      <input type="radio" name="azul" id="azul" value="azul">
+      <label for="azul">
+        <img src="img/produtos/foto2-azul.png" alt="azul">
+      </label>
+
+    </fieldset>
+    <input type="submit" class="comprar" value="Comprar">
+  </form>
+
+
+
+
+
   <?php include("rodape.php") ?>
 </body>
 
