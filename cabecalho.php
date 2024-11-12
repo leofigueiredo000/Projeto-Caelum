53e3d0b944baac2014a249f24bc5f92bc9a44810
Leo Figueiredo <leozim543@gmail.com>
2024-11-12 12:06:28 -0300

    fazendo

--- cabecalho.php
+++ cabecalho.php
@@ -4,11 +4,12 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
- 
+
   <link rel="stylesheet" href="css/reset.css" />
   <link rel="stylesheet" href="css/estilos.css" />
   <link rel="stylesheet" href="css/sobre.css">
   <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+  <link rel="stylesheet" href="css/produto.css">
 
   <title> <?php print $cabecalho_title; ?></title>
 </head>
@@ -31,4 +32,3 @@
       </ul>
     </nav>
   </header>
-</body>
